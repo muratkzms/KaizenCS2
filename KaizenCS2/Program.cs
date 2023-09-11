14df3e3ff230c8547faa438be2ae02ea46d33e0d
Murat Kızmış <murat.kizmis@reateknoloji.com>
2023-09-11 03:16:33 +0300

    Add project files.

---
+++ KaizenCS2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace KaizenCS2
+{
+    internal class Program
+    {
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("response.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Receipt> items = JsonConvert.DeserializeObject<List<Receipt>>(json);
+
+                Console.WriteLine(items[0].description);
+                using (StreamWriter writetext = new StreamWriter("KaizenCS2.txt"))
+                {
+                    writetext.WriteLine(items[0].description);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            LoadJson();
+        }
+    }
+}
---
+++ KaizenCS2/Receipt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaizenCS2
+{
+    public class BoundingPoly
+    {
+        public List<Vertex> vertices { get; set; }
+    }
+    public class Receipt
+    {
+        public string locale { get; set; }
+        public string description { get; set; }
+        public BoundingPoly boundingPoly { get; set; }
+    }
+    public class Vertex
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
